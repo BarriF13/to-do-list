079a4a884fa25e4be7b0084172077d8244ac217f
Barri <info@bdesignstudio.co.uk>
2021-02-06 12:17:16 +0000

    Style the task input section

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import '../style.css'
 export class AddTask extends Component {
   state = {
     taskTitle: ""
@@ -20,8 +20,8 @@ export class AddTask extends Component {
     const { taskTitle } = this.state;
     
     return (
-      <div>
-        <input type="text" value={taskTitle} onChange={onChangeTitle} placeholder="Enter a task tilte"/>
+      <div className='task'>
+        <input className='taskInput' type="text" value={taskTitle} onChange={onChangeTitle} placeholder="Enter a task tilte"/>
         <button disabled={!taskTitle} onClick={onAddTask}>Submit task</button>
       </div>
     )
